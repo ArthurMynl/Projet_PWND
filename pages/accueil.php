729d952d033001993283582bc9304266eea901d6
ArthurMynl <61516506+ArthurMynl@users.noreply.github.com>
2022-05-25 15:12:55 +0200

    avancée accueil

--- pages/accueil.php
+++ pages/accueil.php
@@ -2,9 +2,10 @@
 
 include '../includes/core.php';
 
-$sql = "SELECT etudiant.nom as nomEtudiant, prenom, photo, anneeScolaire.nom as nomAnnee, contenu, dateCreation, auteur
+$sql = "SELECT etudiant.nom as nomEtudiant, prenom, photo, anneeScolaire.nom as nomAnnee, contenu, dateCreation, auteur, media
         FROM etudiant, anneeScolaire, Article
-        WHERE idAnneeScolaire = anneeScolaire AND Article.auteur = etudiant.idEtu";
+        WHERE idAnneeScolaire = anneeScolaire AND Article.auteur = etudiant.idEtu
+        ORDER BY dateCreation DESC";
 $result = $mysqli->query($sql);
 
 ?>
@@ -18,9 +19,9 @@ $result = $mysqli->query($sql);
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
     <title>Page de connexion</title>
     <link rel="stylesheet" href="../style/navbar_style.css">
+    <link rel="stylesheet" href="../style/accueil_style.css">
     <link rel="stylesheet" href="../style/footer_style.css">
 </head>
-
 
 
 <body>
@@ -37,32 +38,34 @@ $result = $mysqli->query($sql);
                         ?>
                         <li> <a href="edit_profil.php">Editer profil</a> </li>
                         <li> <a href="edit_profil.php">Publier un article</a> </li>
-                        <li> <a href="./index.php?logout=1">Deconnexion</a> </li>
+                        <li> <a href="./index.php?logout=1">Déconnexion</a> </li>
                     <?php } ?>
                 </ul>
             </nav>
             <!-- display the articles list -->
             <div class="liste-articles">
-                <h2>Articles</h2>
+                <h2>Derniers articles postés</h2>
                 <?php
                 while ($row = $result->fetch_assoc()) {
                     echo "<div class='article'>";
                         echo "<div class='informations'>";
                             echo "<img src='../assets/" . $row["photo"] . "' class='photo'>";
-                            echo "<h3>" . $row["nomEtudiant"] . " " . $row["prenom"] . " - ". $row["nomAnnee"] . "</h3>";
+                            echo "<h3 class='nomPrenomClasse'>" . $row["nomEtudiant"] . " " . $row["prenom"] . " - " . $row["nomAnnee"] . "</h3>";
                         echo "</div>";
                         echo "<div class='contenu'>";
-                            echo "<p>" . $row["contenu"] . "</p>";
-                            echo "<p>" . $row["dateCreation"] . "</p>";
+                            echo "<p class='texte'>" . $row["contenu"] . "</p>";
+                            echo "<img src='images/assets/" . $row["media"] . "' class='media'>";
+                            echo "<p class='date'>" . $row["dateCreation"] . "</p>";
+                            echo "<p class='heure'>" . $row["dateCreation"] . "</p>";
                         echo "</div>";
                     echo "</div>";
                 }
                 ?>
             </div>
         </div>
+        <footer>
+            <p>Copyright &copy; 2022 - Par Arthur Meyniel - Tous droits réservés</p>
+            <?php $mysqli->close(); ?>
+        </footer>
     </div>
 </body>
-<footer>
-    <p>Copyright &copy; 2022 - Par Arthur Meyniel - Tous droits réservés</p>
-    <?php $mysqli->close(); ?>
-</footer>
