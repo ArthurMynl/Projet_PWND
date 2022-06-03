b20eb326b7de005cb79beada1f0bf97bd0e469e9
ArthurMynl <61516506+ArthurMynl@users.noreply.github.com>
2022-06-03 13:42:18 +0200

    avancement

--- pages/conversation.php
+++ pages/conversation.php
@@ -1,14 +1,15 @@
 <?php
 
+include '../includes/core.php';
+
 $_SESSION['etat'] = 0;
 
 // recuperer les conversations de l'étudiant connecté
-$convSQL = "SELECT Conversation.nom as nomConv FROM Membre, Conversation WHERE Membre.etudiant = " . $_SESSION["compte"] . " AND membre.conversation = conversation.idConv ";
+$convSQL = "SELECT Conversation.nom as nomConv, conversation.idConv as idConv FROM Membre, Conversation WHERE Membre.etudiant = " . $_SESSION["compte"] . " AND membre.conversation = conversation.idConv ";
 $resultConv = $mysqli->query($convSQL);
 
-
 // envoyer un message
-if (isset($_POST['message_submit']) && $_POST['message_submit'] == 1) {
+if (isset($_POST['message_submit'])) {
     // incrémenter l'id du message ?
     $sql = "SELECT MAX(idMess) FROM Message";
     $res = $mysqli->query($sql);
@@ -19,29 +20,44 @@ if (isset($_POST['message_submit']) && $_POST['message_submit'] == 1) {
     $id = $row[0] + 1;
 
     // ajouter le message à la bdd
-    $sql = "INSERT INTO Message (idMess,contenu,dateEnvoi,conversation,emetteur) VALUES (" . $id . ",'" . trim($_POST["contenu"]) . "',NOW()," . $_SESSION['idConvCourante'] . "," . $_SESSION['compte'] . ")";
-    $result = $mysqli->query($sql);
-    if (!$result) {
+    $ajoutMessageSQL = "INSERT INTO Message (idMess,contenu,dateEnvoi,conversation,emetteur) VALUES (" . $id . ",'" . trim($_POST["contenu"]) . "',NOW()," . $_SESSION['idConvCourante'] . "," . $_SESSION['compte'] . ")";
+    $resultAjoutMessage = $mysqli->query($ajoutMessageSQL);
+    if (!$resultAjoutMessage) {
         exit($mysqli->error);
     }
+
+    // récupérer les messages de la conversation
+    $messageSQL = "SELECT contenu, Etudiant.nom as nomEmetteur, Etudiant.prenom as prenomEmetteur, emetteur, dateEnvoi FROM Message, Etudiant WHERE Message.emetteur = Etudiant.idEtu AND conversation = " . $_SESSION['idConvCourante'] . " ORDER BY dateEnvoi ASC";
+    $resultMessage = $mysqli->query($messageSQL);
+
     unset($_POST["message_submit"]);
 }
 
 // selectionne la conversation 
 if (isset($_POST['conv_courante'])) {
     $_SESSION['idConvCourante'] = $_POST['conv_courante'];
-    $messageSQL = "SELECT contenu,emetteur,dateEnvoi FROM Message WHERE idMess =" . $row["idMess"];
+    $messageSQL = "SELECT contenu, Etudiant.nom as nomEmetteur, Etudiant.prenom as prenomEmetteur, emetteur, dateEnvoi FROM Message, Etudiant WHERE Message.emetteur = Etudiant.idEtu AND conversation = " . $_SESSION['idConvCourante'] . " ORDER BY dateEnvoi ASC";
     $resultMessage = $mysqli->query($messageSQL);
+    $participantSQL = "SELECT Etudiant.nom as nomEtudiant, Etudiant.prenom as prenomEtudiant FROM Etudiant, Conversation, Membre WHERE Etudiant.idEtu = Membre.etudiant AND Membre.conversation = Conversation.idConv AND Conversation.idConv = " . $_SESSION['idConvCourante'];
+    $resultParticipant = $mysqli->query($participantSQL);
+    unset($_POST["conv_courante"]);
+}
+
+// selectionne le mode
+if (isset($_POST['etat'])) {
+    $_SESSION['etat'] = $_POST['etat'];
 }
 
 // créer la conversation
 if (isset($_POST['creation_submit']) && $_POST['creation_submit'] == 1) {
     $_SESSION['etat'] = 1;
+    unset($_POST["creation_submit"]);
 }
 
 // ajouter des personnes
 if (isset($_POST['ajout_submit']) && $_POST['ajout_submit'] == 1) {
     $_SESSION['etat'] = 2;
+    unset($_POST["creation_submit"]);
 }
 
 // valider la creation de la conversation
@@ -57,7 +73,7 @@ if (isset($_POST['validation_creation']) && $_POST['validation_creation'] == 1) 
 
 
     // ajouter la conversation à la bdd
-    $sql = "INSERT INTO Conversation (idConv,Nom,dateCreation) VALUES (" . $id . ",'" . trim($_POST["nom"]) . "',NOW() )";
+    $sql = "INSERT INTO Conversation (idConv,Nom,dateCreation) VALUES (" . $id . ",'" . trim($_POST["nom_conv"]) . "',NOW() )";
     $result = $mysqli->query($sql);
     if (!$result) {
         exit($mysqli->error);
@@ -70,7 +86,7 @@ if (isset($_POST['validation_creation']) && $_POST['validation_creation'] == 1) 
         exit($mysqli->error);
     }
 
-    // recuperer les conversations de l'étudiant connecté
+    // récuperer les conversations de l'étudiant connecté
     $convSQL = "SELECT conversation FROM Membre WHERE etudiant = " . $_SESSION["compte"];
     $resultConv = $mysqli->query($convSQL);
 
@@ -78,12 +94,15 @@ if (isset($_POST['validation_creation']) && $_POST['validation_creation'] == 1) 
         exit($mysqli->error);
     }
 
-    $_SESSION['etat'] == 0;
+    $_SESSION['idConvCourante'] = $id;
+    $_SESSION['etat'] = 0;
+    unset($_POST["validation_creation"]);
 }
 
 // annuler nouvelle conversation
 if (isset($_POST['annulation_creation']) && $_POST['annulation_creation'] == 1) {
-    $_SESSION['etat'] == 0;
+    $_SESSION['etat'] = 0;
+    unset($_POST["annulation_creation"]);
 }
 
 // ajouter des personnes 
@@ -111,27 +130,30 @@ if (isset($_POST['validation_ajout']) && $_POST['validation_ajout'] == 1) {
         if (!$result) {
             exit($mysqli->error);
         }
-        $_SESSION['etat'] == 0;
+        $_SESSION['etat'] = 0;
+        unset($_POST["validation_ajout"]);
     }
 }
 
 // annuler ajouter une personne
 if (isset($_POST['annulation_ajout']) && $_POST['annulation_ajout'] == 1) {
-    $_SESSION['etat'] == 0;
+    $_SESSION['etat'] = 0;
+    unset($_POST["annulation_ajout"]);
 }
 
 ?>
 
 <!DOCTYPE html>
-
-<html lang="fr">
+<html lang="en">
 
 <head>
-    <meta charset="utf-8">
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Accueil</title>
     <link rel="stylesheet" href="../style/navbar_style.css">
     <link rel="stylesheet" href="../style/conversation_style.css">
     <link rel="stylesheet" href="../style/footer_style.css">
-    <title><?php echo $_TITRE_PAGE ?></title>
 </head>
 
 <body>
@@ -140,7 +162,7 @@ if (isset($_POST['annulation_ajout']) && $_POST['annulation_ajout'] == 1) {
             <nav class="navbar">
                 <ul>
                     <li> <img src="../assets/logo.png" id="logo"> </li>
-                    <li> <a href="/pages/index.php" class="active">Accueil</a> </li>
+                    <li> <a href="/pages/index.php">Accueil</a> </li>
                     <li> <a href="/pages/Etudiants.php">Étudiants</a> </li>
                     <?php if ($_SESSION["compte"]) { ?>
                         <?php
@@ -150,7 +172,7 @@ if (isset($_POST['annulation_ajout']) && $_POST['annulation_ajout'] == 1) {
                         <li> <a href="articles.php">Publier un article</a> </li>
                         <li> <a href="amis.php">Amis</a> </li>
                         <?php
-                        echo "<li> <a href='liste_conversation.php?id=" . $_SESSION["compte"] . "'> Conversation </a> </li>"; ?>
+                        echo "<li> <a href='liste_conversation.php?id=" . $_SESSION["compte"] . "' class='active'> Conversation </a> </li>"; ?>
                         <li> <a href="./index.php?logout=1">Déconnexion</a> </li>
                     <?php } ?>
                 </ul>
@@ -159,49 +181,90 @@ if (isset($_POST['annulation_ajout']) && $_POST['annulation_ajout'] == 1) {
             <nav class="small-nav">
                 <form method="post">
                     <ul>
-                        <li> <input class="small-nav-btn">Mes conversations</input></li>
-                        <li> <input class="small-nav-btn">Créer une Conversation</input></li>
-                        <li> <input class="small-nav-btn">Ajouter des personnes à cette conversation</input></li>
+                        <li> <button class="small-nav-btn" type="submit" name='etat' value="0">Mes conversations</button></li>
+                        <li> <button class="small-nav-btn" type="submit" name='etat' value="1">Ajouter des personnes à cette conversation</button></li>
+                        <li> <button class="small-nav-btn" type="submit" name='etat' value="2">Créer une Conversation</button></li>
                     </ul>
                 </form>
             </nav>
 
             <?php if ($_SESSION["etat"] == 0) { ?>
 
-                <h1><?php echo $_SESSION["etat"] ?></h1>
+                <h1>Mes conversations</h1>
                 <div id="liste-conversations">
                     <?php while ($conversation = $resultConv->fetch_assoc()) { ?>
-                        <div class="appercu-conversation">
-                            <?php echo $conversation['nomConv'] ?>
-                            <form method='post'>
-                                    <input type='submit'>voir conversation</input>
-                            </form>
-                        </div>
+                        <form method='post'>
+                            <button type='submit' name="conv_courante" value='<?php echo $conversation['idConv'] ?>'><?php echo $conversation['nomConv'] ?></button>
+                        </form>
                     <?php } ?>
                 </div>
-                <?php if (isset($convCourante)) { ?>
+                <?php if (isset($_SESSION['idConvCourante'])) { ?>
                     <div class="conversation">
-                        <div class="messages">
-                            <?php while ($message = $resultMessage->fetch_assoc()) { 
-                                if($message['emetteur'] == $_SESSION['compte']){ ?>
-                                    <div class="message-sortant">
-                                        <h3 class="message-emetteur"><?php echo $message['emetteur'] ?></h3>
-                                        <p class="message-sortant"> <?php echo $message['contenu'] ?> </p>
-                                        <h6> <?php echo $message['date'] ?> </h6>
-                                    </div>
-                                <?php } else { ?>
-                                    <div class="message-entrant">
-                                        <h3 class="message-emetteur"><?php echo $message['emetteur'] ?></h3>
-                                        <p> <?php echo $message['contenu'] ?> </p>
-                                        <h6> <?php echo $message['date'] ?> </h6>
-                                    </div>
+                        <div class='page-messages'>
+                            <div class="messages">
+                                <?php
+                                if (isset($resultMessage)) {
+                                    while ($message = $resultMessage->fetch_assoc()) {
+                                        if ($message['emetteur'] == $_SESSION['compte']) { ?>
+                                            <div class="message-sortant">
+                                                <h3 class="message-emetteur"><?php echo $message['prenomEmetteur'] . " " . $message['nomEmetteur'] ?></h3>
+                                                <p class="message-sortant"> <?php echo $message['contenu'] ?> </p>
+                                                <h6> <?php echo $message['dateEnvoi'] ?> </h6>
+                                            </div>
+                                        <?php } else { ?>
+                                            <div class="message-entrant">
+                                                <h3 class="message-emetteur"><?php echo $message['prenomEmetteur'] . " " . $message['nomEmetteur'] ?></h3>
+                                                <p> <?php echo $message['contenu'] ?> </p>
+                                                <h6> <?php echo $message['dateEnvoi'] ?> </h6>
+                                            </div>
+                                        <?php } ?>
+                                    <?php } ?>
                                 <?php } ?>
+                            </div>
+                            <form class="envoi-message">
+                                <input type="text" name="message" placeholder="Votre message">
+                                <input type="submit" value="Envoyer" name="message_submit">
+                            </form>
+                        </div>
+                        <div class='participants'>
+                            <h3>Participants</h3>
+                            <?php while ($participant = $resultParticipant->fetch_assoc()) { ?>
+                                <div class="participant">
+                                    <h4><?php echo $participant['prenomEtudiant'] . " " . $participant['nomEtudiant'] ?></h4>
+                                </div>
                             <?php } ?>
                         </div>
-                        <form class="envoi-message">
-                            <input type="text" name="message" placeholder="Votre message">
-                            <input type="submit" value="Envoyer">
+                    </div>
+                <?php } ?>
+            <?php } else if ($_SESSION['etat'] == 1) { ?>
+                <?php if (isset($_SESSION['idConvCourante'])) { ?>
+                    <div class="ajout-personne">
+                        <h1>Ajouter une personne à la conversation</h1>
+                        <form method="post">
+                            <input type="text" name="mail" placeholder="Adresse mail de l'étudiant"></input>
+                            <div class="boutons">
+                                <button name="validation_ajout" value="1" type="submit">Ajouter</button>
+                                <button name="annulation_ajout" value="1" type="submit">Annuler</button>
+                            </div>
                         </form>
                     </div>
                 <?php } ?>
+            <?php } else if ($_SESSION['etat'] == 2) { ?>
+                <div class="creation-conversation">
+                    <h1>Créer une conversation</h1>
+                    <form method="post">
+                        <input type="text" name="nom_conv" placeholder="Nom de la conversation"></input>
+                        <div class="boutons">
+                            <button name="validation_creation" value="1" type="submit">Créer</button>
+                            <button name="annulation_creation" value="1" type="submit">Annuler</button>
+                        </div>
+                    </form>
+                </div>
             <?php } ?>
+        </div>
+        <footer>
+            <p>Copyright &copy; 2022 - Par Arthur Meyniel - Tous droits réservés</p>
+            <?php $mysqli->close(); ?>
+        </footer>
+    </div>
+</body>
